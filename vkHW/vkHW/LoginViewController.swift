f1a4f2ddb18a3bda23cb6f6dacdb653f441a957f
Александр <decomp30000012@mail.ru>
2022-11-02 10:03:09 +0300

    исправил замечания

--- vkHW/vkHW/LoginViewController.swift
+++ vkHW/vkHW/LoginViewController.swift
@@ -12,6 +12,8 @@ final class LoginViewController: UIViewController {
         static let titleAlertText = "Заполните все данные"
         static let messageAlertText = "Вы должны заполнить пароль и логин"
         static let alertActionText = "Окей"
+        static let loginText = "admin"
+        static let passwordText = "12345"
     }
 
     // MARK: - IBOutlets
@@ -53,24 +55,39 @@ final class LoginViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
 
+    // MARK: - Public Methods
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == Constants.segueIdentifier {
+            guard let loginText = loginTextField.text,
+                  let passwordText = passwordTextField.text
+            else { return false }
+            if loginText == Constants.loginText, passwordText == Constants.passwordText {
+                performSegue(withIdentifier: Constants.segueIdentifier, sender: nil)
+                return true
+            } else {
+                errorAlert(title: Constants.titleAlertText, message: Constants.messageAlertText)
+                return false
+            }
+        }
+        return false
+    }
+
     // MARK: - IBActions
 
     @IBAction private func logInButtonAction(_ sender: Any) {
-        if loginTextField.text?.isEmpty == false, passwordTextField.text?.isEmpty == false {
-            performSegue(withIdentifier: Constants.segueIdentifier, sender: nil)
-        } else {
-            errorAlert(title: Constants.titleAlertText, message: Constants.messageAlertText)
-        }
+        shouldPerformSegue(withIdentifier: Constants.segueIdentifier, sender: nil)
     }
 
     // MARK: - Private Methods
 
     @objc private func keyboardWillShown(notification: Notification) {
-        guard let info = notification.userInfo as NSDictionary? else { return }
-        guard let kbSize = (
-            info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey)
-                as? NSValue?
-        )??.cgRectValue.size else { return }
+        guard let info = notification.userInfo as NSDictionary?,
+              let kbSize = (
+                  info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey)
+                      as? NSValue?
+              )??.cgRectValue.size else { return }
+
         let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.height, right: 0)
         loginScrollView.contentInset = contentInset
         loginScrollView.scrollIndicatorInsets = contentInset
